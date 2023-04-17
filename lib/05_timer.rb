7eea9d32afaa010bab2f269f988c300c71faecbd
theonapoly <theo.napoly@gmail.com>
2023-04-18 00:00:32 +0200

    modifs exo5 time_string

--- lib/05_timer.rb
+++ lib/05_timer.rb
@@ -1,3 +1,7 @@
-def time_string(n)
-    return "%02d:%02d:%02d" % [n / 3600, n / 60%60, n %60]
-end
+def time_string(seconds)
+    hours = seconds / 3600
+    minutes = (seconds % 3600) / 60
+    seconds = seconds % 60
+  
+    return format("%02d:%02d:%02d", hours, minutes, seconds)
+  end
